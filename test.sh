1d198e279aaefd6d83ffdda8021c7219795b9882
pratap76 <105881559+pratap76@users.noreply.github.com>
2022-11-27 23:16:28 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
-echo "This is my first project"
-date
-time
-cat
+#!/bin/bash
+echo "what is your age"
+read age
+if [ "$age" -ge 18 ]; then
+echo "you are eligible to vote"
+else 
+echo "you are younger !!"
